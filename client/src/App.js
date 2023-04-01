2f8733d15b95512a1dc559a527dd1cd0923b2f11
Jakub Stadnik <106155603+jakubstadnik03@users.noreply.github.com>
2023-04-01 16:28:57 +0200

    k

---
+++ client/src/App.js
@@ -0,0 +1,32 @@
+import NavbarComponent from "./components/navbar/NavbarComponent";
+import "bootstrap/dist/css/bootstrap.css";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Recipe from "./pages/Recipe/Recipe";
+import Home from "./pages/Home/Home";
+import Data from "./data/data.json";
+import NewRecipe from "./pages/NewRecipe/NewRecipe";
+import Footer from "./components/Footer";
+function App() {
+  const recipeList = Data.map((data) => data);
+  return (
+    <div className="App">
+      <NavbarComponent />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/pridat-recept" element={<NewRecipe />} />
+
+        {recipeList.map((item) => (
+          <Route
+            path={`/recept/${item.id}`}
+            key={item.id}
+            element={<Recipe data={item} />}
+          />
+        ))}
+      </Routes>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/pages/Home/Home.js
@@ -0,0 +1,8 @@
+import React from "react";
+import RecipeList from "../../components/RecipeList";
+
+const Home = () => {
+  return <RecipeList />;
+};
+
+export default Home;
